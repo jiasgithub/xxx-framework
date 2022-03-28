aa0a813de5c22f3b7f0843655a04644e37f6252a
1874 <jias@allsaintsmusic.com>
2022-03-28 19:33:03 +0800

    first

---
+++ study-framework/src/main/java/com/songjia/study/StudyFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.songjia.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyFrameworkApplication.class, args);
+    }
+
+}
---
+++ study-framework/src/test/java/com/songjia/study/StudyFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songjia.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
